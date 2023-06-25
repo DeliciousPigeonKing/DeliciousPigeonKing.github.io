0bd4de4c8e4c1a246e6f7411ad21fdf952e8a426
Delicious-PigeonKing <2833818457@qq.com>
2023-06-25 20:40:40 +0800

    Site updated: 2023-06-25 20:40:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/05/02/PigeonKingの博客升级！/","2023/05/02/arcaea双星纪念/","2023/05/02/中考加油/","2023/04/30/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/05/02/PigeonKingの博客升级！/","2023/05/02/arcaea双星纪念/","2023/04/30/hello-world/","2023/05/02/中考加油/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
